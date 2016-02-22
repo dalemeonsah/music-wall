8a3ac5bbacdbcb1073e061ec88a6aea758e98995
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-02-22 21:51:57 +0000

    similar to sinatra first app exercise

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,33 @@
 get '/' do
   erb :index
 end
+
+get '/tracks' do
+  @tracks = Track.all
+  erb :'tracks/index_track'
+end
+
+get '/tracks/new' do
+  @track = Track.new
+  erb :'tracks/new_track'
+end
+
+get '/tracks/:id' do
+  @track = Track.find(params[:id])
+  @all_track = Track.all
+  erb :'tracks/show_track'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    song_title: params[:song_title],
+    url: params[:url],
+    author: params[:author]  
+  )
+
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new_track'
+  end
+end
